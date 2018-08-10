4fe0faa0925ca18ebb3c05289497f100f7720ffd
Miggy Reyes <ynigoreyes@gmail.com>
2018-08-10 18:30:50 -0500

    moving Content up

--- src/layout/Content/ContentContainer.jsx
+++ src/layout/Content/ContentContainer.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import Content from './components';
+import { Route, Redirect } from 'react-router-dom';
 
+import Events from 'pages/Events';
+import Activity from 'pages/Activity';
+import Settings from 'pages/Settings';
+
+// Handles all the routing which will display the routes in the body of the page
 const ContentContainer = () => (
-  <Content />
+  <React.Fragment>
+    {/* Load the events page by default */}
+    <Route
+      exact
+      path='/'
+      render={() => (
+        <Redirect to='/events' />
+      )}
+    />
+
+    {/* Set the children of the primary component */}
+    <Route path='/events' component={Events} />
+    <Route path='/activity' component={Activity} />
+    <Route path='/settings' component={Settings} />
+  </React.Fragment>
 );
 
 export default ContentContainer;

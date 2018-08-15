a2fd19074fbc6778f61ad64b8018a6ff658258f4
Miggy Reyes <ynigoreyes@gmail.com>
2018-08-15 16:33:06 -0500

    moved to connected-react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import { HashRouter } from 'react-router-dom';
+import { ConnectedRouter as Router } from 'connected-react-router';
 import { Provider } from 'react-redux';
 
 import { NavBarContainer, ContentContainer } from 'layout/';
-import store from 'redux/store.js';
+import store, { history } from 'redux/store.js';
 
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
@@ -20,12 +20,12 @@ const theme = createMuiTheme({
 const App = () => (
   // Provider shares store with components joined by connect()
   <Provider store={store}>
-    <HashRouter>
+    <Router history={history}>
       <MuiThemeProvider theme={theme}>
         <NavBarContainer />
         <ContentContainer />
       </MuiThemeProvider>
-    </HashRouter>
+    </Router>
   </Provider>
 );
 
--- src/hoc/index.js
+++ src/hoc/index.js
@@ -1,2 +1,1 @@
 export { default as FormChecker } from './FormChecker.jsx';
-export { default as RequireAuth } from './RequireAuth.jsx';
